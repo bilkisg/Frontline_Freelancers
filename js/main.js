5c3795adf9e4cd6a740ace4d654d45d98c52d501
bilkisg <bilkisg@hotmail.com>
2016-11-24 21:12:42 +0000

    second revision

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+$(document).ready(function(){
+  $(".showForm1").click(function(){
+    
+      $(".searchForm1").fadeToggle();
+  });
+
+  $(".showForm2").click(function(){
+
+      $(".searchForm2").fadeToggle();
+  });
+
+  $('[data-toggle="tooltip"]').tooltip(); 
+
+ 
+  $(".carousel-inner").click(function(){
 
 
+      //var w = window.open("file:///C:/Users/bilki/Desktop/Frontline_Freelancers/about.html","_self");
+      //w.location = "../about.html"; 
+      
+  });
 
+  $('.mylink').click(function(event) {
+
+    // This will prevent the default action of the anchor and prevent jumping whilst the page has empty links
+    event.preventDefault();
+
+ 
+
+});
+  
+});
